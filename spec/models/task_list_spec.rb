08deef4c79187881faed1dd4f938f8a47ce09622
wintter <chesterfield252@gmail.com>
2015-08-27 13:59:47 +0300

    add spec to task_list model

--- spec/models/task_list_spec.rb
+++ spec/models/task_list_spec.rb
@@ -6,4 +6,13 @@ RSpec.describe TaskList, type: :model do
   it { expect(subject).to belong_to(:project) }
   it { expect(subject).to have_many(:comments) }
 
+  describe '#task_user' do
+    let(:subject) { FactoryGirl.create(:task_list) }
+
+    it 'return user' do
+      expect(subject.task_user).to be_an_instance_of User
+    end
+
+  end
+
 end
