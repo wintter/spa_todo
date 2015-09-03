375cb1c13a5418ef9911ab1436b6f2d8ace4b6da
wintter <chesterfield252@gmail.com>
2015-09-03 15:35:49 +0300

    config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,10 +13,6 @@ RSpec.configure do |config|
   end
 
   config.before(:suite) do
-    # This says that before the entire test suite runs, clear the test database out completely.
-    # This gets rid of any garbage left over from interrupted or poorly-written tests - a common source of surprising test behavior.
-    DatabaseCleaner.clean_with(:truncation)
-
     # This part sets the default database cleaning strategy to be transactions.
     # Transactions are very fast, and for all the tests where they do work - that is, any test where the entire test runs in the RSpec process - they are preferable.
     DatabaseCleaner.strategy = :transaction
